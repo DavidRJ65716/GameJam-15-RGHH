250248005d0f94fb39c741d1d0e6a49ddf44f6e9
DavidRJ65716 <daverj65716@gmail.com>
2024-07-27 09:27:07 -0500

    Testing sprites movment

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,14 @@
-import { Stage, Container, Text } from '@pixi/react';
-import { TextStyle, BlurFilter } from 'pixi.js';
-import { useEffect, useMemo, useState } from 'react';
-import { Bunny } from './sprites/bunny';
-import './App.css'
+import { Stage } from '@pixi/react';
+import MainApp from './page/MainApp';
 import { useReSizeContext } from './contexts/reSizeContext';
-
-
-const style = new TextStyle( {
-  align: "center",
-  fontWeight: '800',
-  dropShadow: true,
-  dropShadowAlpha: 0.6,
-  dropShadowAngle: 1,
-  dropShadowBlur: 5,
-  wordWrap: true,
-  fill: ["white", "#cccccc"]
-})
 
 function App() {
   
   const {width, height} = useReSizeContext()
   
   return (
-    <Stage width={width} height={height} raf={false} options={{ backgroundColor: 0x1099bb, resizeTo: window}}>
-      <Container position={[100,100]}>
-        <Bunny />
-      </Container>
-      <Text
-            text={`${width}\nx\n${height}`}
-            style={style}
-            anchor={0.5}
-            x={width / 2}
-            y={height / 2}
-          />
+    <Stage width={width} height={height} raf={false} options={{ backgroundColor: 0x1099bb, resizeTo: window, antialias: true}}>
+      <MainApp />
     </Stage>
   )
 }
--- src/sprites/bunny.tsx
+++ src/sprites/bunny.tsx
@@ -1,45 +1,45 @@
-import { Sprite, AnimatedSprite, useTick,render } from "@pixi/react"; 
-import { useState,useEffect } from "react";
+import { useTick } from "@pixi/react";
+import { Application, ICanvas, Texture, Sprite } from "pixi.js";
+import { useEffect } from "react";
 
-export function Bunny () {
-  // states
-  const [x, setX] = useState(500);
-  const [y, setY] = useState(500);
-  const [rotation, setRotation] = useState(0);
+interface keys {
+  [key: string] : boolean
+}
 
-  useEffect(() => {
+export function Bunny (app: Application<ICanvas>) {
+   // states
 
-    const keyDown = (e: KeyboardEvent) => {
-      switch(e.key){
-        case 'w': setY(y-1);
-        break
-        case 's': setY(y+1);
-        break
-        case 'a': setX(x-1);
-        break
-        case 'd': setX(x+1);
-        break
-      }
-    }
+   const player = new Sprite(Texture.from("https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png"))
+   player.anchor.set(0.5)
+   player.x = app.screen.width/2
+   player.y = app.screen.height/2
+   player.scale.set(5,5)
+   const keyPress : keys = {'w':false,'s':false,'a':false,'d':false, ' ':false}
+
+   app.stage.addChild(player)
+ 
+   useEffect(() => {
 
     window.addEventListener("keydown", keyDown)
-  },[])
+    window.addEventListener("keyup", keyUp)
+    console.log(JSON.stringify(keyPress))
+  },[ keyPress])
 
-  function gameLoop(){
-
+  const keyDown = (e: KeyboardEvent) => {
+    keyPress[e.key] = true
   }
 
+  const keyUp = (e: KeyboardEvent) => {
+    keyPress[e.key] = false
+  }
 
-    
-  console.log(y + " " +x)
+  useTick(gameLoop)
 
-  return (
-    <Sprite
-      image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png"
-      anchor={0.5}
-      x={x}
-      y={y}
-      rotation={rotation}
-    />
-  );
+  function gameLoop(){
+    if(keyPress['w']) player.y -= 5
+    if(keyPress['s']) player.y += 5
+    if(keyPress['a']) player.x -= 5
+    if(keyPress['d']) player.x += 5
+    if(keyPress[' ']) player.y -= 5
+  }
 };
